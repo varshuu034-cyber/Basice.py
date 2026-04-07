1e2116906344634d7fa53c4179a5905d861bd4c2
Varshini.V <varshuu034@gmail.com>
2026-04-07 21:48:13 +0530

    Add files via upload

---
+++ basice.py
@@ -0,0 +1,19 @@
+while True:
+    print("Enter your choice")
+    print("1. Add expense")
+    print("2. view All expenses")
+    print("3. viwe total expenses")
+    print("4. Exit")
+
+    choice = input("Enter your chice")
+
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    else:
+        print("Exiting from the porject ! Good bye!")
+        break
+              
